52c59a0d9c9831c90adf70ecad9a1cbfb760d5a8
john <redford.john.m@gmail.com>
2015-07-13 15:53:02 -0500

    fade effect on change in value

--- client/index.js
+++ client/index.js
@@ -3,14 +3,19 @@ var webSock = new WebSocket('ws://mvp-mks18-redford.herokuapp.com');
 //var webSock = new WebSocket('ws://127.0.0.1:3000')
 
 
-var NUM_COLS = 80;
+var NUM_COLS = Math.floor(window.innerWidth/16);
+var NUM_ROWS = Math.floor(window.innerHeight/18);
 var columnArray = [];
 
 window.onload = function(){
   console.log('document loaded, making columns');
   for (var i = 0; i < NUM_COLS; i++){
-    columnArray.push($('<div>').addClass('column').appendTo(document.body));
-
+    columnArray.push({
+      characterDomContainer: $('<div>').addClass('column').appendTo(document.body),
+      characters:[],
+      current:0,
+      filledOnce:false
+    });
   }  
 }
 
@@ -20,13 +25,33 @@ webSock.onerror = function(err){
 };
 
 webSock.onopen = function (event) {
-  console.log('sending message from client to server');
-  webSock.send("Message from client to server"); 
+  console.log('socket open'); 
 };
 
 webSock.onmessage = function(e){
 
-  var newDiv = $('<div>').html(e.data);
   var n = Math.floor(Math.random()*NUM_COLS);
-  newDiv.appendTo(columnArray[n]);
+  if (!columnArray[n].filledOnce){
+    var newDiv = $('<div>').html(e.data);
+    newDiv.appendTo(columnArray[n].characterDomContainer);
+    columnArray[n].characters.push(newDiv);
+    columnArray[n].current++;
+    if (columnArray[n].current > NUM_ROWS){
+      columnArray[n].filledOnce = true;
+      columnArray[n].current = 0;
+    }
+  } else {
+    if (columnArray[n].current < NUM_ROWS){
+      columnArray[n].characters[columnArray[n].current].remove()
+      var newTag = $('<div>').html(e.data);
+      columnArray[n].characters[columnArray[n].current+1].before(newTag);
+      columnArray[n].characters[columnArray[n].current] = newTag;
+    }
+
+    columnArray[n].characters[columnArray[n].current].html(e.data);
+    columnArray[n].current++;
+    if (columnArray[n].current > NUM_ROWS) {
+      columnArray[n].current = 0;
+    }
+  }
 };
